79f5d25d681a28585c4b5784c47612c0d90a9de4
SY122095 <90448533+SY122095@users.noreply.github.com>
2022-07-07 17:02:38 +0900

    Update main2.py

--- main2.py
+++ main2.py
@@ -38,50 +38,49 @@ def main():
     loggedin = login()
     if loggedin:
         st.write('Authenticated!')
-            
+        # タイトルの表示
+        st.title('Streamlit 超入門')
 
+        st.write('プログレスバーの表示')
+        'Start!!'
+        latest_iteration = st.empty()
+        bar = st.progress(0)
+        for i in range(100):
+            latest_iteration.text(f'Iteration{i+1}')
+            bar.progress(i+1)
+            time.sleep(0.1)
+        'Done!'
+
+
+        st.write('Interactive Widgets')
+        left_column, right_column = st.columns(2)
+        button = left_column.button('右カラムに文字を表示')
+        if button:
+            right_column.write('ここは右カラム')
+
+        expander = st.expander('問い合わせ')
+        expander.write('問い合わせ内容を書く')
+
+        text = st.text_input('あなたの趣味を教えてください。')
+        condition = st.slider('あなたの今の調子は？', 0, 100, 50)
+
+        'あなたの趣味は', text, 'です。'
+        'コンディション: ', condition
+
+        option = st.selectbox(
+            'あなたが好きな数字を教えてください。',
+            list(range(1, 11))
+        )
+        'あなたの好きな数字は、', option, 'です。'
+
+        if st.checkbox('Show Image'):
+            img = Image.open('C:\\Users\\yhfhg\\OneDrive\\デスクトップ\\streamlit\\mlb.jpg')
+            st.image(img, caption='MLB', use_column_width=True)
+        
 if __name__ == '__main__':
     main()
 
 # Streamlitの実行
 # streamlit run -----.py
 
-# タイトルの表示
-if get_hash(password) == hashed_password:
-    st.title('Streamlit 超入門')
-
-    st.write('プログレスバーの表示')
-    'Start!!'
-    latest_iteration = st.empty()
-    bar = st.progress(0)
-    for i in range(100):
-        latest_iteration.text(f'Iteration{i+1}')
-        bar.progress(i+1)
-        time.sleep(0.1)
-    'Done!'
-
-
-    st.write('Interactive Widgets')
-    left_column, right_column = st.columns(2)
-    button = left_column.button('右カラムに文字を表示')
-    if button:
-        right_column.write('ここは右カラム')
-
-    expander = st.expander('問い合わせ')
-    expander.write('問い合わせ内容を書く')
-
-    text = st.text_input('あなたの趣味を教えてください。')
-    condition = st.slider('あなたの今の調子は？', 0, 100, 50)
-
-    'あなたの趣味は', text, 'です。'
-    'コンディション: ', condition
-
-    option = st.selectbox(
-        'あなたが好きな数字を教えてください。',
-        list(range(1, 11))
-    )
-    'あなたの好きな数字は、', option, 'です。'
-
-    if st.checkbox('Show Image'):
-        img = Image.open('C:\\Users\\yhfhg\\OneDrive\\デスクトップ\\streamlit\\mlb.jpg')
-        st.image(img, caption='MLB', use_column_width=True)
+    
