c11b96f0497155a7913a0059d5c14de8f96f0691
SY122095 <90448533+SY122095@users.noreply.github.com>
2022-07-07 17:00:34 +0900

    Update main2.py

--- main2.py
+++ main2.py
@@ -47,7 +47,7 @@ if __name__ == '__main__':
 # streamlit run -----.py
 
 # タイトルの表示
-if login():
+if get_hash(password) == hashed_password:
     st.title('Streamlit 超入門')
 
     st.write('プログレスバーの表示')
