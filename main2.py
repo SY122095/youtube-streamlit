f5e916ff9d1655f03e17d831b69a6cd2660fc3ca
SY122095 <90448533+SY122095@users.noreply.github.com>
2022-07-07 14:30:08 +0900

    Update main2.py

--- main2.py
+++ main2.py
@@ -4,6 +4,21 @@ import pandas as pd
 import time
 from PIL import Image
 
+# Everything is accessible via the st.secrets dict:
+
+st.write("DB username:", st.secrets["db_username"])
+st.write("DB password:", st.secrets["db_password"])
+st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
+
+# And the root-level secrets are also accessible as environment variables:
+
+import os
+
+st.write(
+    "Has environment variables been set:",
+    os.environ["db_username"] == st.secrets["db_username"],
+)
+
 # Streamlitの実行
 # streamlit run -----.py
 
